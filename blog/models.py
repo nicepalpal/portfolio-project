9670c404f32313de4f4dd5b153e093c1532857ba
nicepalpal <nicepalpal@gmail.com>
2019-03-09 09:57:57 +0900

    mac

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,11 @@ class Blog(models.Model):
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
 
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
 
 
 # Create A Blog models
