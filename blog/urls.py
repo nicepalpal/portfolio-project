fb49ed2db12666ccbe7b31ddd96b8399d5af7e47
Andrew Pelizzaro <nicepalpal@andoryunoMacBook-Pro.local>
2019-03-24 15:50:37 +0900

    home page done

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.allblogs, name='home' )
+    path('', views.allblogs, name='bloghome' ),
+    #look for an int after the /blog and we will save as blog id
+    path('<int:blog_id>/', views.detail, name='detail')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Blog
 
 def allblogs(request):
     blogs = Blog.objects
     return render(request, 'blog/allblogs.html', {'blogs': blogs})
+
+def detail(request, blog_id):
+    #uses above import of get object 404 to grab object or get_object_or_404
+    #Pk is primary key, takes blog_id from urls.py and checks DB for all blogs)
+    detailblog = get_object_or_404(Blog, pk=blog_id)
+    return render (request, 'blog/detail.html', {'blog': detailblog})
